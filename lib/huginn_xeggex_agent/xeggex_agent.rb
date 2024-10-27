8f0b92a288cdb7bbf30422ef964049edac6ca08f
Nicolas Germain <ngermain@hihouhou.com>
2024-10-27 22:16:53 +0100

    check_orders added

--- lib/huginn_xeggex_agent/xeggex_agent.rb
+++ lib/huginn_xeggex_agent/xeggex_agent.rb
@@ -17,6 +17,12 @@ module Agents
 
       `api_secret` is needed for endpoints with auth.
 
+      `symbol` is needed for queries like get_orders, for example CLO/BTC
+
+      `status` is needed for queries like get_orders and limited to active, filled and cancelled.
+
+      `limit` is needed for queries like get_orders.
+
       `expected_receive_period_in_days` is used to determine if the Agent is working. Set it to the maximum number of days
       that you anticipate passing without this Agent receiving an incoming Event.
       MD
@@ -41,25 +47,45 @@ module Agents
         'type' => 'check_balance',
         'api_token' => '',
         'api_secret' => '',
+        'symbol' => '',
+        'status' => 'filled',
+        'limit' => '10',
         'debug' => 'false',
         'expected_receive_period_in_days' => '2',
       }
     end
 
-    form_configurable :type, type: :array, values: ['check_balance']
+    form_configurable :type, type: :array, values: ['check_balance', 'check_orders']
     form_configurable :api_key, type: :string
     form_configurable :api_secret, type: :string
+    form_configurable :symbol, type: :string
+    form_configurable :status, type: :array, values: ['active', 'filled', 'cancelled']
+    form_configurable :limit, type: :string
     form_configurable :debug, type: :boolean
     form_configurable :expected_receive_period_in_days, type: :string
     def validate_options
-      errors.add(:base, "type has invalid value: should be 'check_balance'") if interpolated['type'].present? && !%w(check_balance).include?(interpolated['type'])
+      errors.add(:base, "type has invalid value: should be 'check_balance' 'check_orders'") if interpolated['type'].present? && !%w(check_balance check_orders).include?(interpolated['type'])
 
-      unless options['api_key'].present? || !['check_balance'].include?(options['type'])
+      errors.add(:base, "status has invalid value: should be 'active' 'filled' 'cancelled'") if interpolated['type'].present? && !%w(active filled cancelled).include?(interpolated['status'])
+
+      unless options['api_key'].present? || !['check_balance', 'check_orders'].include?(options['type'])
         errors.add(:base, "api_key is a required field")
       end
 
-      unless options['api_secret'].present? || !['check_balance'].include?(options['type'])
+      unless options['api_secret'].present? || !['check_balance', 'check_orders'].include?(options['type'])
         errors.add(:base, "api_secret is a required field")
+      end
+
+      unless options['symbol'].present? || !['check_orders'].include?(options['type'])
+        errors.add(:base, "symbol is a required field")
+      end
+
+      unless options['status'].present? || !['check_orders'].include?(options['type'])
+        errors.add(:base, "status is a required field")
+      end
+
+      unless options['limit'].present? || !['check_orders'].include?(options['type'])
+        errors.add(:base, "limit is a required field")
       end
 
       if options.has_key?('debug') && boolify(options['debug']).nil?
@@ -96,10 +122,65 @@ module Agents
       log "request status : #{code}"
 
       if interpolated['debug'] == 'true'
-        log "request status : #{code}"
         log "body"
         log body
       end
+
+    end
+
+    def check_orders(xeggex_url_endpoint)
+
+      uri = URI.parse(xeggex_url_endpoint + '/getorders?symbol=' + interpolated['symbol'] + '&status=' + interpolated['status'] + '&limit=' + interpolated['limit'] + '&skip=0')
+      request = Net::HTTP::Get.new(uri)
+      request.basic_auth("#{interpolated['api_key']}", "#{interpolated['api_secret']}")
+      request["Accept"] = "application/json"
+      
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+      
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+
+      log_curl_output(response.code,response.body)
+
+      payload = JSON.parse(response.body)
+      payload_memory = payload.dup
+      if payload != memory['last_status']
+        payload.each do |order|
+          found = false
+          if interpolated['debug'] == 'true'
+            log "order"
+            log order
+          end
+          if !memory['last_status'].nil? and memory['last_status'].present?
+            if interpolated['debug'] == 'true'
+              log "memory"
+              log memory['last_status']
+            end
+            last_status = memory['last_status']
+            last_status.each do |orderbis|
+              if order == orderbis
+                found = true
+              end
+              if interpolated['debug'] == 'true'
+                log "orderbis"
+                log orderbis
+                log "found is #{found}!"
+              end
+            end
+          end
+          if found == false
+            create_event payload: order
+          end
+        end
+      else
+        if interpolated['debug'] == 'true'
+          log "nothing to compare"
+        end
+      end
+      memory['last_status'] = payload_memory
 
     end
 
@@ -166,6 +247,8 @@ module Agents
       case interpolated['type']
       when "check_balance"
         check_balance(xeggex_url_endpoint)
+      when "check_orders"
+        check_orders(xeggex_url_endpoint)
       else
         log "Error: type has an invalid value (#{type})"
       end
